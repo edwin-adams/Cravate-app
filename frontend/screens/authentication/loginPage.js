6b423a7880f58c0a2d3b046e39f2a5aaa511c79f
Het Mehta <ht883100@dal.ca>
2023-03-17 01:07:29 -0300

    Added validation

--- frontend/screens/authentication/loginPage.js
+++ frontend/screens/authentication/loginPage.js
@@ -1,10 +1,25 @@
-import React from "react";
-import { SafeAreaView, StyleSheet, View } from "react-native";
+import React,{ useState } from "react";
+import { SafeAreaView, StyleSheet, View, Text } from "react-native";
 import { Button, Card, TextInput } from "react-native-paper";
 import { useForm, Controller, SubmitHandler } from "react-hook-form"
 
 
 export const LoginScreen = ({navigation}) => {
+    
+    const [showPassword, setShowPassword] = useState(false);
+    const { control, handleSubmit, formState: { errors } } = useForm();
+
+    // const onSubmit = async (data) => {
+    //     try {
+    //     const response = await axios.post('http://yourbackendurl.com/login', data);
+    //     console.log(response.data);
+    //     } catch (error) {
+    //     console.error(error);
+    //     }
+    // };
+    const onSubmit = (data) => {
+        console.log(data);
+    }
     
     return (
         <SafeAreaView style={styles.container}>
@@ -12,9 +27,59 @@ export const LoginScreen = ({navigation}) => {
                 <Card>
                     <Card.Title title = "Cravate" titleStyle= {styles.title}></Card.Title>
                     <Card.Content>
-                        <TextInput label = "username" keyboardType="email-address"  left={<TextInput.Icon name="account" />} style={styles.textinput}></TextInput>
-                        <TextInput label = "password" left={<TextInput.Icon name="form-textbox-password" />} right = {<TextInput.Icon name = "eye-off-outline"/>}  secureTextEntry={true} style={styles.textinput}></TextInput>
-                        <Button mode="contained" style={styles.button}>Login</Button>
+
+                        <Controller
+                            control={control}
+                            rules={{
+                            required: true,
+                            }}
+                            render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                label="Username"
+                                onBlur={onBlur}
+                                style={styles.textinput}
+                                onChangeText={onChange}
+                                value={value}
+                                error={!!errors.username}
+                            />
+                            )}
+                            name="username"
+                            defaultValue=""
+                        />
+
+                        {errors.username && (
+                            <Text>Username cannot be empty</Text>
+                        )}
+
+                        <Controller
+                            control={control}
+                            rules={{
+                            required: true,
+                            minLength: 6,
+                            }}
+                            render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                label="Password"
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                style={styles.textinput}
+                                value={value}
+                                error={!!errors.password}
+                                secureTextEntry={!showPassword}
+                                right={<TextInput.Icon name={showPassword ? 'eye-off' : 'eye'} onPress={() => setShowPassword(!showPassword)} />}
+                            />
+                            )}
+                            name="password"
+                            defaultValue=""
+                        />
+
+                        {errors.password && errors.password.type === 'minLength' && (
+                            <Text>Password must be at least 6 characters long</Text>
+                        )}
+
+                        {/* <TextInput label = "username" keyboardType="email-address"  left={<TextInput.Icon name="account" />} style={styles.textinput}></TextInput>
+                        <TextInput label = "password" left={<TextInput.Icon name="form-textbox-password" />} right = {<TextInput.Icon name = "eye-off-outline"/>}  secureTextEntry={true} style={styles.textinput}></TextInput> */}
+                        <Button mode="contained" style={styles.button} onPress={handleSubmit(onSubmit)}>Login</Button>
                         <Button uppercase={false} style={styles.button} onPress={() => navigation.navigate("register")}>Don't have an account? Sign Up here</Button>
                     </Card.Content>
                 </Card>
